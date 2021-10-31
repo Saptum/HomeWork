b2915ea47af846178840c3c67f3236cb9da2dff7
Hydrus <61348601+Saptum@users.noreply.github.com>
2021-10-31 21:17:09 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,5 +3,10 @@ package HomeWork_3;
 public class Main {
     public static void main(String[] args) {
 
+             IntList myList = new IntArrayList();
+
+        for (int i = 0; i < 100000; i++) {
+            myList.add(i);
+        }
     }
 }
