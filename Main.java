bc2fc6020cf128108205bd2a5a33e74fe441bc42
Hydrus <61348601+Saptum@users.noreply.github.com>
2021-11-01 16:40:54 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+package HomeWork_3;
+
+public class Main {
+    public static void main(String[] args) {
+        IntList myList = new IntArrayList();
+
+        for (int i = 0; i < 100000; i++) {
+            myList.add(i);
+        
+        }
+    }
+}
